87a2db2194a4478d4c68d97c208da0f4685d3acf
Sachin Lomte <sachinlomte2008@gmail.com>
2017-03-31 14:26:04 +0530

    Mygrievances prototype view

---
+++ app/src/main/java/com/example/gra/gra/ListAdapter.java
@@ -0,0 +1,58 @@
+package com.example.gra.gra;
+
+/**
+ * Created by sachin on 31/3/17.
+ */
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+
+public class ListAdapter extends ArrayAdapter<JSONObject>{
+
+    int vg;
+    ArrayList<JSONObject> list;
+    Context context;
+
+    public ListAdapter(Context context, int vg, int id, ArrayList<JSONObject> list){
+
+        super(context,vg, id,list);
+
+        this.context=context;
+        this.vg=vg;
+        this.list=list;
+
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View itemView = inflater.inflate(vg, parent, false);
+
+        TextView txtId=(TextView)itemView.findViewById(R.id.txtgid);
+        TextView txtCategory=(TextView)itemView.findViewById(R.id.txtcategory);
+        TextView txtStatus=(TextView)itemView.findViewById(R.id.txtstatus);
+
+        try {
+
+            txtId.setText(list.get(position).getString("g_id"));
+            txtCategory.setText(list.get(position).getString("category"));
+            txtStatus.setText(list.get(position).getString("status"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return itemView;
+
+    }
+
+}
--- app/src/main/java/com/example/gra/gra/MyGrievanceActivity.java
+++ app/src/main/java/com/example/gra/gra/MyGrievanceActivity.java
@@ -1,22 +1,60 @@
 package com.example.gra.gra;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class MyGrievanceActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.support.v7.app.ActionBarActivity;
+
+import java.util.ArrayList;
+
+public class MyGrievanceActivity extends ActionBarActivity {
 
     //handle my grievnance
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_grievance);
 
-        final TextView response1=(TextView) findViewById(R.id.tvMyGrievance);
         Intent intent = getIntent();
         String answer = intent.getStringExtra("response_array");
-        response1.setText(answer);
+
+        JSONArray jsonArray=null;
+        try {
+            jsonArray = new JSONArray(answer);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        ArrayList<JSONObject> listItems=getArrayListFromJSONArray(jsonArray);
+
+        ListView listV=(ListView)findViewById(R.id.lvGrievance);
+
+        ListAdapter adapter=new ListAdapter(this,R.layout.list_layout,R.id.txtgid,listItems);
+
+        listV.setAdapter(adapter);
+
+    }
+
+    private ArrayList<JSONObject> getArrayListFromJSONArray(JSONArray jsonArray){
+        ArrayList<JSONObject> aList=new ArrayList<JSONObject>();
+        try {
+            if (jsonArray != null) {
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    aList.add(jsonArray.getJSONObject(i));
+                }
+            }
+
+        }catch (JSONException je){je.printStackTrace();}
+
+        return  aList;
     }
 }
