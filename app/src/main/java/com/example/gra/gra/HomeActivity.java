02eeb5c7e4406f29d14562f5ab99ced43afd3583
Sachin Lomte <sachinlomte2008@gmail.com>
2017-03-31 12:04:17 +0530

    logo added in register and home

--- app/src/main/java/com/example/gra/gra/HomeActivity.java
+++ app/src/main/java/com/example/gra/gra/HomeActivity.java
@@ -9,6 +9,13 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.content.DialogInterface;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
@@ -31,6 +38,48 @@ public class HomeActivity extends AppCompatActivity {
                 HomeActivity.this.startActivity(complaintIntent);
             }
         });
+
+        bMyComplaint.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                final String aadhar_number = MyGlobalVariable.getMyAadharNumber();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response){
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            //boolean success = jsonObject.getBoolean("success");
+                            Intent intent = new Intent(HomeActivity.this,MyGrievanceActivity.class);
+                            //JSONArray movies = jsonObject.getJSONArray("category");
+                            //String hey = movies.toString();
+                            intent.putExtra("response_array",response);
+                            HomeActivity.this.startActivity(intent);
+                            /*if (success){
+
+                                Intent intent = new Intent(HomeActivity.this,MyGrievanceActivity.class);
+                                HomeActivity.this.startActivity(intent);
+
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
+                                builder.setMessage("My Complaint Request Failed")
+                                        .setNegativeButton("Retry",null)
+                                        .create()
+                                        .show();
+                            }*/
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                MyGrievanceRequest grievanceRequest = new MyGrievanceRequest(aadhar_number,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(HomeActivity.this);
+                queue.add(grievanceRequest);
+            }
+        });
+
 
         bMyprofile.setOnClickListener(new View.OnClickListener(){
             @Override
