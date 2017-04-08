c182fca8577d720826ee962deeeb2695ca32c92f
raviprakash25x <raviprakash.25x@gmail.com>
2017-04-08 13:36:14 -0700

    Created new file for my grievances

--- app/src/main/java/com/example/gra/gra/govtOffcHomeActivity.java
+++ app/src/main/java/com/example/gra/gra/govtOffcHomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.gra.gra;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class govtOffcHomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class govtOffcHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_govt_offc_home);
+
+        Button bViewGrievance = (Button) findViewById(R.id.btViewGrievances);
+        Button bBlockUser = (Button) findViewById(R.id.btBlockUser);
+        Button bUpdateEmergency = (Button) findViewById(R.id.btUpdateEmergency);
+
+        bViewGrievance.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent registerIntent = new Intent(govtOffcHomeActivity.this,GOGrievanceActivity.class);
+                govtOffcHomeActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        bBlockUser.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent registerIntent = new Intent(govtOffcHomeActivity.this,GOGrievanceActivity.class);
+                govtOffcHomeActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        bUpdateEmergency.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent registerIntent = new Intent(govtOffcHomeActivity.this,GOEmergencyActivity.class);
+                govtOffcHomeActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
