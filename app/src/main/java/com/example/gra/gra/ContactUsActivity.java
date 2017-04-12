8a4296b35fd11123954f6e43e8d1ca84b95bc63d
ranjithrajvasam <ranjithraj.vasam@gmail.com>
2017-04-13 02:51:58 +0530

    Changed UI and Added functinality to ContactUsActivity

--- app/src/main/java/com/example/gra/gra/ContactUsActivity.java
+++ app/src/main/java/com/example/gra/gra/ContactUsActivity.java
@@ -1,7 +1,16 @@
 package com.example.gra.gra;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.R.id.message;
 
 public class ContactUsActivity extends AppCompatActivity {
 
@@ -9,5 +18,33 @@ public class ContactUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        final Button button = (Button) findViewById(R.id.button);
+        final Button button2 = (Button) findViewById(R.id.button2);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto", "email@email.com", null));
+
+                startActivity(Intent.createChooser(intent, "Choose an Email client :"));
+
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:7737486232"));
+
+                startActivity(callIntent);
+            }
+        });
     }
+
 }
